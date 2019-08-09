7eeb68a8c13eee91ff12ddd35bcb579bf9dc4567
MegaKreja <kreja95ue@gmail.com>
2019-08-09 15:09:55 +0200

    header finished

--- index.php
+++ index.php
@@ -5,13 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel = "stylesheet" type = "text/css" href = "style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre:400,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800&display=swap" rel="stylesheet">
+
+
     <title>Wordpress theme with chairs</title>
   </head>
   <body>
     <div class="container">
       <div class="header">
-        <div class="header.nav">
-          
+        <div class="header-nav-text">
+          <div class="header-nav">
+            <ul>
+              <li><img class="logo" src="images/logo.png" alt="logo"></li>
+              <li><a class="active" href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Production</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+          <div class="header-text">
+            <h1>Otpresci za stolice</h1>
+            <hr class="hr">
+            <p>Nudimo kvalitetan i raznovrsan<br> asortiman poluproizvoda za izradu stolica,<br> sedišta, rukonaslone, školjke, itd.</p>
+            <button>Proizvodi</button>
+          </div>
         </div>
       </div>
     </div>
