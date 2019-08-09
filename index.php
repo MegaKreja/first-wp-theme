41b46d6e55328cb973280aab3c58dc95d02f5448
MegaKreja <kreja95ue@gmail.com>
2019-08-09 16:07:06 +0200

    about us nearly finished images left

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   <body>
     <div class="container">
       <div class="header">
-        <div class="header-nav-text">
+        <div class="header-nav-text content">
           <div class="header-nav">
             <ul>
               <li><img class="logo" src="images/logo.png" alt="logo"></li>
@@ -32,6 +32,39 @@
             <p>Nudimo kvalitetan i raznovrsan<br> asortiman poluproizvoda za izradu stolica,<br> sedišta, rukonaslone, školjke, itd.</p>
             <button>Proizvodi</button>
           </div>
+        </div>
+      </div>
+      <div class="aboutUs">
+        <div class="aboutUs-content content">
+          <h1 class="aboutUs-header">About us</h1>
+            <hr>
+          <div class="aboutUs-info">
+            <div>
+              <h1>Our Company</h1>
+              <br>
+              <p>Established in 2001, ZVEMIL is a family company, base in western part of Serbia, in a small but beautiful town near border with Bosnia. SInce 2002 it has been producing quality plywood seating compnents from bech veneer for the contract seating industry. Numerous schools, cinema and theater halls, hotels and restaurants across Serbia and region are equiped with seatings that include our chair components.</p>
+            </div>
+            <div>
+              <h1>Our Mission</h1>
+              <br>
+              <p>Our mission is to stay quality and reliable producer of curved plywood seatings and furniture components, continually raising our technical and technology level of production in order to meet the expectations of out clients.<br><br>"Help you to get best product you require"</p>
+            </div>
+            <div>
+              <h1>Our Values</h1>
+              <br>
+              <p>Hard work, dedication, focus and a strong commitment, focus and a strong commitment to respecting deadlines, make ZVEMIL has almost a 20-year history of manufacturing quality molded plywood products, and i trusted partner in the production for our clients in seating industry.</p>
+            </div>
+            <div>
+              <h1>Our Goals</h1>
+              <br>
+              <p>As an environmentally conscious company ZVEMIL strives to meet the following goals: <br>
+              <br>-Minimize waste by evaluating operations and ensuring they are as efficient as possible,<br>
+              -Recycling wood and metal waste,<br>
+              -Minimize harmful emissions from our adhesives by using the lowest emission resins available on the market.
+              </p>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
