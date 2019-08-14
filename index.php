e0728d846848a8c5658219cb53861bcf3cdeaf89
MegaKreja <kreja95ue@gmail.com>
2019-08-14 15:23:18 +0200

    bootstrap finished

--- index.php
+++ index.php
@@ -8,29 +8,37 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre:400,700,800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/ab3130ba7b.js"></script>
 
     <title>Wordpress theme with chairs</title>
   </head>
   <body>
-    <div class="container">
-      <div class="header">
-        <div class="header-nav-text content">
-          <div class="header-nav">
-            <ul>
-              <li><img class="logo" src="images/logo.png" alt="logo"></li>
-              <li><a class="active" href="#">Home</a></li>
-              <li><a href="#">About us</a></li>
-              <li><a href="#">Production</a></li>
-              <li><a href="#">Products</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </div>
-          <div class="header-text">
+    <div class="container-fluid">
+      <div class="header row">
+        <div class="header-nav-text container">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#"><img src="images/logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav ml-auto">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">About Us</a>
+                <a class="nav-item nav-link" href="#">Production</a>
+                <a class="nav-item nav-link" href="#">Products</a>
+                <a class="nav-item nav-link" href="#">Contact</a>
+              </div>
+            </div>
+          </nav>
+          <div class="header-text text-center text-md-left">
             <h1>Otpresci za stolice</h1>
-            <hr class="hr">
+            <hr class="hr mx-auto m-md-0">
             <p>Nudimo kvalitetan i raznovrsan<br> asortiman poluproizvoda za izradu stolica,<br> sedišta, rukonaslone, školjke, itd.</p>
-            <button>Proizvodi</button>
+            <button type="button" class="btn btn-outline-dark">Proizvodi</button>
           </div>
         </div>
       </div>
@@ -38,24 +46,24 @@
         <div class="aboutUs-content content">
           <h1 class="content-header">About us</h1>
           <hr class="content-hr">
-          <div class="aboutUs-info four-inline">
-            <div>
-              <h1 class="content-text-header">Our Company</h1>
+          <div class="aboutUs-info row">
+            <div class="col-lg-6 col-xl-3">
+              <h1 class="content-text-header text-center text-lg-left">Our Company</h1>
               <br>
-              <p class="content-paragraph">Established in 2001, ZVEMIL is a family company, base in western part of Serbia, in a small but beautiful town near border with Bosnia. SInce 2002 it has been producing quality plywood seating compnents from bech veneer for the contract seating industry. Numerous schools, cinema and theater halls, hotels and restaurants across Serbia and region are equiped with seatings that include our chair components.</p>
+              <p class="content-paragraph">Established in 2001, ZVEMIL is a family company, base in western part of Serbia, in a small but beautiful town near border with Bosnia. Since 2002 it has been producing quality plywood seating compnents from bech veneer for the contract seating industry. Numerous schools, cinema and theater halls, hotels and restaurants across Serbia and region are equiped with seatings that include our chair components.</p>
             </div>
-            <div>
-              <h1 class="content-text-header">Our Mission</h1>
+            <div class="col-lg-6 col-xl-3">
+              <h1 class="content-text-header text-center text-lg-left">Our Mission</h1>
               <br>
               <p class="content-paragraph">Our mission is to stay quality and reliable producer of curved plywood seatings and furniture components, continually raising our technical and technology level of production in order to meet the expectations of out clients.<br><br>"Help you to get best product you require"</p>
             </div>
-            <div>
-              <h1 class="content-text-header">Our Values</h1>
+            <div class="col-lg-6 col-xl-3">
+              <h1 class="content-text-header text-center text-lg-left">Our Values</h1>
               <br>
               <p class="content-paragraph">Hard work, dedication, focus and a strong commitment, focus and a strong commitment to respecting deadlines, make ZVEMIL has almost a 20-year history of manufacturing quality molded plywood products, and i trusted partner in the production for our clients in seating industry.</p>
             </div>
-            <div>
-              <h1 class="content-text-header">Our Goals</h1>
+            <div class="col-lg-6 col-xl-3">
+              <h1 class="content-text-header text-center text-lg-left">Our Goals</h1>
               <br>
               <p class="content-paragraph">As an environmentally conscious company ZVEMIL strives to meet the following goals: <br>
               <br>-Minimize waste by evaluating operations and ensuring they are as efficient as possible,<br>
@@ -64,14 +72,14 @@
               </p>
             </div>
           </div>
-          <div class="aboutUs-images">
-            <img class="images-three" src="images/placeholder.gif" alt="placeholder">
-            <img class="images-three" src="images/placeholder.gif" alt="placeholder">
-            <img class="images-three" src="images/placeholder.gif" alt="placeholder">
+          <div class="aboutUs-images row mx-auto">
+            <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
+            <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
+            <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
           </div>
         </div>
       </div>
-      <div class="middle-grey">
+      <div class="middle-grey row">
         <div class="content content-middle-grey">
           <h1 class="content-header">Production</h1>
           <hr class="content-hr">
@@ -81,20 +89,20 @@
         </div>
       </div>
       <div class="production-info content">
-        <div class="production-info-images">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
+        <div class="production-info-images row mx-auto">
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
         </div>
         <div class="production-info-text">
           <h1 class="content-text-header">CNC Woodworking</h1>
           <p class="content-paragraph">Once the molded plywood panels are pressed we machine them on Bacci 5-axis CNC machining-centre. This sophisticated computer controlled router allow us to produce variety of consistenly uniform and concise upholstery and expsosed grade components</p>
         </div>
 
-        <div class="production-info-images">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
+        <div class="production-info-images row mx-auto">
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
         </div>
         <div class="production-info-text">
           <h1 class="content-text-header">Design Services</h1>
@@ -103,10 +111,10 @@
           <p class="content-paragraph">We can offer technology and knowledge to help the client design and manufacture prototype. Once the client is satisfied with the prototype, we use a Microscribe arm to create a precise 3D digital model and finally transfer the design to our CNC machine.</p>
         </div>
 
-        <div class="production-info-images">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
-          <img class="images-three" src="images/placeholder.gif" alt="placeholder">
+        <div class="production-info-images row mx-auto">
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
+          <div class="col-md-4"><img class="images-three img-fluid" src="images/placeholder.gif" alt="placeholder"></div>
         </div>
         <div class="production-info-text production-info-bottom">
           <h1 class="content-text-header">Production capacity & Quality</h1>
@@ -114,7 +122,7 @@
           <p class="content-paragraph">&middot; Plywood thickness range is 4.5-40mm in form, quality and finishing scale requested by the client.</p>
         </div>
       </div>
-      <div class="middle-grey">
+      <div class="middle-grey row">
         <div class="content content-middle-grey">
           <h1 class="content-header">Products</h1>
           <hr class="content-hr">
@@ -122,63 +130,60 @@
         </div>
       </div>
       <div class="products-info content">
-        <div class="products-info-images">
-          <img src="images/stolica.jpg" alt="stolica">
-          <img src="images/stolica.jpg" alt="stolica">
-          <img src="images/stolica.jpg" alt="stolica">
-          <img src="images/stolica.jpg" alt="stolica">
-        </div>
-        <div class="products-info-text four-inline">
-          <div>
+        <div class="products-info-images row mx-auto">
+          <div class="col-md-6 col-lg-3">
+            <img class="img-fluid" src="images/stolica.jpg" alt="stolica">
             <h1 class="content-text-header">Nasloni za stolice</h1>
             <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo excepturi sit nihil, facere molestiae explicabo quis optio in! Tempore architecto hic quas soluta, id saepe odio nemo iusto voluptate.</p>
           </div>
-          <div>
+          <div class="col-md-6 col-lg-3">
+            <img class="img-fluid" src="images/stolica.jpg" alt="stolica">
             <h1 class="content-text-header">Školjke stolica</h1>
             
-            <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo excepturi sit nihil, facere molestiae explicabo quis optio in! Tempore architecto hic quas soluta, id saepe odio nemo iusto voluptate.</p>
-          </div>
-          <div>
+            <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo excepturi sit nihil, facere molestiae explicabo quis optio in! Tempore architecto hic quas soluta, id saepe odio nemo iusto voluptate.</p></div>
+          <div class="col-md-6 col-lg-3">
+            <img class="img-fluid" src="images/stolica.jpg" alt="stolica">
             <h1 class="content-text-header">Rukonasloni za stolice</h1>
             
             <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo excepturi sit nihil, facere molestiae explicabo quis optio in! Tempore architecto hic quas soluta, id saepe odio nemo iusto voluptate.</p>
           </div>
-          <div>
+          <div class="col-md-6 col-lg-3">
+            <img class="img-fluid" src="images/stolica.jpg" alt="stolica">
             <h1 class="content-text-header">Sedišta za stolice</h1>
             
-            <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo excepturi sit nihil, facere molestiae explicabo quis optio in! Tempore architecto hic quas soluta, id saepe odio nemo iusto voluptate.
-            </p>
+            <p class="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo excepturi sit nihil, facere molestiae explicabo quis optio in! Tempore architecto hic quas soluta, id saepe odio nemo iusto voluptate.</p>
+          </div>
+          <div class="moto mx-auto">
+            <h1 class="content-text-header">Moto</h1>
+            <p class="content-paragraph">"With our hard work and knowledge we can firmly support the design of your chairs"</p>
           </div>
         </div>
-        <div class="moto">
-          <h1 class="content-text-header">Moto</h1>
-          <p class="content-paragraph">"With our hard work and knowledge we can firmly support the design of your chairs"</p>
+      </div>
+      <div class="contact-info middle-grey row">
+        <div class="col-12 col-lg-2"></div>
+        <div class="content contact-info-text col-12 col-lg-4">
+          <h1 class="content-header text-center text-lg-left">Zvemil d.o.o</h1>
+          <hr class="content-hr mx-auto m-lg-0">
+          <p class="content-paragraph text-center text-lg-left phone-margin"><i class="fas fa-phone-alt"></i>  031 861551</p>
+          <p class="content-paragraph text-center text-lg-left"><i class="fas fa-mobile-alt"></i>  064 8267313</p>
+          <p class="content-paragraph text-center text-lg-left"><i class="fas fa-map-marker-alt"></i>  Dragoljuba Bešlića, Bajina Bašta 31250, Srbija</p>
+          <p class="content-paragraph text-center text-lg-left"><i class="fas fa-clock"></i>  Pon - Pet: <strong>06:00 - 14:00</strong></p>
+          <p class="content-paragraph text-center text-lg-left">Sub - Ned: <strong>Ne radi</strong></p>
+        </div>
+        <div class="contact-info-image col-12 col-lg-6">
+          <img class="img-fluid" src="images/mapa (2).PNG" alt="mapa">            
         </div>
       </div>
-      <div class="contact-info middle-grey">
-        <div class="content contact-info-text">
-          <h1 class="content-header">Zvemil d.o.o</h1>
-          <hr class="content-hr">
-          <p class="content-paragraph"><i class="fas fa-phone-alt"></i>  031 861551</p>
-          <p class="content-paragraph"><i class="fas fa-mobile-alt"></i>  064 8267313</p>
-          <p class="content-paragraph"><i class="fas fa-map-marker-alt"></i>  Dragoljuba Bešlića, Bajina Bašta 31250, Srbija</p>
-          <p class="content-paragraph"><i class="fas fa-clock"></i>  Pon - Pet: <strong>06:00 - 14:00</strong></p>
-          <p class="content-paragraph">Sub - Ned: <strong>Ne radi</strong></p>
-        </div>
-        <div class="contact-info-image">
-          <img src="images/mapa.PNG" alt="mapa">            
-        </div>
-      </div>
-      <div class="footer content">
-        <div class="footer-nav">
-          <ul>
-            <li><a class="active" href="#">Home</a></li>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><img class="logo" src="images/logo.png" alt="logo"></li>
-            <li>&copy; 2019 Helendo. All rights reserved.</li>
-          </ul>
+      <div class="footer content container">
+        <div class="footer-nav row align-items-center">
+          <div class="col-12 col-lg-3">
+            <a class="active" href="#">Home</a>
+            <a href="#">Products</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </div>
+          <div class="col-12 col-lg-6 order-first order-lg-0"><img class="logo" src="images/logo.png" alt="logo"></div>
+          <div class="col-12 col-lg-3">&copy; 2019 Helendo. All rights reserved.</div>
         </div>
       </div>
     </div>
